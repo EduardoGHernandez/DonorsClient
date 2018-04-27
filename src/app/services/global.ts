228c55c5443bd25160c3d10d00a54caa237b22bf
Eduardo Hernandez <egh1795@gmail.com>
2018-04-27 14:53:51 -0500

    Change in url api

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,5 @@
 export var GLOBAL = {
-  url : 'http://localhost:3800/api/'
-   //url : 'http://localhost:3800/api/'
+  url:'https://morning-lowlands-75402.herokuapp.com/api/'
+  //url : 'http://localhost:3800/api/'
+  //url : 'http://localhost:3800/api/'
 }
